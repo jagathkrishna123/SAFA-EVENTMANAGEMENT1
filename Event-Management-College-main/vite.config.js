ca2572397436353c28a7541f1966fb475c7149b1
jagathkrishna123 <jagathkrishna900@gmail.com>
2026-02-23 14:48:40 +0530

    last commit

--- Event-Management-College-main/vite.config.js
+++ Event-Management-College-main/vite.config.js
@@ -7,32 +7,41 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr(),
-  VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.svg'],
-    manifest: {
-      name: 'Attendance QR App',
-      short_name: 'Attendance',
-      description: 'Employee Attendance QR Scanner',
-      theme_color: '#0f172a',
-      background_color: '#0f172a',
-      display: 'standalone',
-      orientation: 'portrait',
-      start_url: '/',
-      scope: '/',
-      icons: [
-        {
-          src: '/pwa-192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/pwa-512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
-  })]
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+      },
+      manifest: {
+        name: 'SAFA Event Management',
+        short_name: 'SAFA',
+        description: 'Employee Attendance QR Scanner',
+        theme_color: '#0f172a',
+        background_color: '#0f172a',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
